96d703e3832f8e01e3e97cdf18c018abfa95e988
PieterVanBoheemen <pietervanboheemen@gmail.com>
2015-04-14 10:59:17 +0200

    fixed communication and libraries in separate folder

--- arduino/PeristalticPump/PeristalticPump.ino
+++ arduino/PeristalticPump/PeristalticPump.ino
@@ -228,7 +228,7 @@ void loop() {
       if(buffer.startsWith("id")) {
         Serial.println("id:peristaltic-pump"); // so the bioreactor can figure out what is connected 
       } else if (buffer.startsWith("move")) {
-        float sp = buffer.substring(2).toInt()/60.0f;
+        float sp = buffer.substring(4).toInt()/60.0f;
         Serial.print(F("Setting motor speed to "));
         Serial.print(sp, 2);
         Serial.println(F(" rpm"));
