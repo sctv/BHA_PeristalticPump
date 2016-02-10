887f30018fc83029d9f934fc5e72fed74314d78b
PieterVanBoheemen <pieter@waag.org>
2016-02-10 14:58:47 +0100

    updating pins

--- Arduino Code/PeristalticPump/PeristalticPump.ino
+++ Arduino Code/PeristalticPump/PeristalticPump.ino
@@ -26,118 +26,111 @@ move <speed> Set continuous pump rotation speed (rpm)
 
 */
 
-/* *******************************************************
-/  Libraries
-*/
+
 #include <Wire.h> // Needed for I2C connection
 #include "LiquidCrystal_I2C.h" // Needed for operating the LCD screen
 #include <TimerOne.h>
 #include <SoftwareSerial.h>
-/* *******************************************************
-*/
 
-/* *******************************************************
-// Define pins
-*/
 #define LED_PIN 13      // the number of Arduino's onboard LED pin
 
-#define MOTOR_STEP_PIN    4
-#define MOTOR_DIR_PIN     5
-#define MOTOR_SLEEP_PIN   6
+class RotaryEncoder
+{
+  byte state;
+  byte pin0, pin1;
+  int value;
 
-// These have to be interrupt pins (see http://arduino.cc/en/Reference/attachInterrupt). For the UNO, this means we have to use pin 2 and 3
-#define ROTARY_ENCODER_PINA 2
-#define ROTARY_ENCODER_PINB 3
+  byte readState()
+  {
+    return (digitalRead(pin0) == HIGH ? 1u : 0u)
+         | (digitalRead(pin1) == HIGH ? 2u : 0u);
+  }
+public:
+  RotaryEncoder(int p0, int p1) :
+    pin0(p0), pin1(p1), value(0), state(0) {}
+  
+  void init()
+  {
+    pinMode(pin0, INPUT);
+    pinMode(pin1, INPUT);
+    digitalWrite(pin0, 1);  // enable internal pullup
+    digitalWrite(pin1, 1);  // enable internal pullup
+    value = 0;
+    state = readState();
+  }
 
-/* *******************************************************
-*/
+  bool poll()
+  {
+    // State transition table
+    static char tbl[16] =
+    { 0, +1, -1, 0,
+      // position 3 = 00 to 11, can't really do anythin, so 0
+      -1, 0, -2, +1,
+      // position 2 = 01 to 10, assume a bounce, should be 01 -> 00 -> 10
+      +1, +2, 0, -1,
+      // position 1 = 10 to 01, assume a bounce, should be 10 -> 00 -> 01
+      0, -1, +1, 0
+      // position 0 = 11 to 10, can't really do anything
+    };
+  
+    byte t = readState();
+    char movement = tbl[(state << 2) | t];
+    if (movement != 0)
+    {
+      value += movement;
+      state = t;
+      return true;
+    }
+    return false;
+  }
+  
+  int getValue()
+  {
+    return value;
+  }
+};
 
-/* *******************************************************
-/  LCD
-*/
+RotaryEncoder rotaryEncoderA (2,3);
+RotaryEncoder rotaryEncoderB (A2,A3);
+
+
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27,16,2);
-/* *******************************************************
-*/
 
 byte ledstate = false; // Blinking indicator LED
 
-/* *******************************************************
-/  Time variables
-*/
-uint32_t lastTick = 0; 
-uint32_t lastUpdate=0;
-
-/* Useful Constants */
-#define SECS_PER_MIN  (60UL)
-#define SECS_PER_HOUR (3600UL)
-#define SECS_PER_DAY  (SECS_PER_HOUR * 24L)
- 
-/* Useful Macros for getting elapsed time */
-#define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)  
-#define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN)
-#define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
-#define elapsedDays(_time_) ( _time_ / SECS_PER_DAY)  
-/* *******************************************************
-*/
-
-/* *******************************************************
-/  Stepper variables
-*/
-float motorSpeed = 0.0f;
-#define MICROSTEPS 32
-volatile bool continuousMode = false;
-volatile int stepsTodo = 0;
-/* *******************************************************
-*/
-
-/* *******************************************************
-/  Rotary encoder variables
-*/
-//Based on bildr article: http://bildr.org/2012/08/rotary-encoder-arduino/
-volatile int lastEncoded = 0;
-volatile long encoderValue = 0;
-long lastencoderValue = 0;
-int lastMSB = 0;
-int lastLSB = 0;
-/* *******************************************************
-*/
-
-/* *******************************************************
-/  Serial interface
-*/
 String buffer;
-/* *******************************************************
-*/
+byte readout;
 
-/* *******************************************************
-/  updateEncoder is the function that reacts to the rotary encoder interrupts
-*/
-void updateEncoder(){
-  int MSB = digitalRead(ROTARY_ENCODER_PINA); //MSB = most significant bit
-  int LSB = digitalRead(ROTARY_ENCODER_PINB); //LSB = least significant bit
+long last_time;
+int stepper_timer=0;
+int step_time = 0;
+long steps=0;
+int  update_timer=0;
 
-  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
-  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+byte stepper_en = 0;
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+const byte PIN_STEP=7; 
+const byte PIN_ENABLE=4;
+const byte PIN_DIR=8;
 
-  lastEncoded = encoded; //store this value for next time
-}
+const byte PIN_MOSFET0 = 5;
+const byte PIN_MOSFET1 = 6;
 
-/* *******************************************************
-/  setupRotaryEncoder is the function that initiates the coder
-*/
-void setupRotaryEncoder() {
-  // Set pinmodes
-  pinMode(ROTARY_ENCODER_PINA, INPUT); 
-  pinMode(ROTARY_ENCODER_PINB, INPUT);
 
-  //call updateEncoder() when any high/low changed seen
-  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)  (see http://arduino.cc/en/Reference/attachInterrupt)
-  attachInterrupt(0, updateEncoder, CHANGE); 
-  attachInterrupt(1, updateEncoder, CHANGE);
+int mosfet_switch_timer=0;
+byte mosfet_index=0;
+
+
+void scanI2C()
+{
+  for (int i=0;i<127;i++) {
+    Wire.beginTransmission(i);
+    byte err = Wire.endTransmission();
+    if(err == 0) {
+      Serial.print("Device at: " ); Serial.println(i, HEX);
+    }
+  }
 }
 
 void setupLCD() {
@@ -153,13 +146,34 @@ void setupLCD() {
   lcd.print(F("Peristaltic Pump"));
 }
 
+void setup() {
+  // Open serial connection and print a message
+  Serial.begin(9600);
+  Serial.println(F("BioHackAcademy Peristaltic Pump"));
+
+  rotaryEncoderA.init();
+  rotaryEncoderB.init();
+  
+  pinMode(PIN_STEP, OUTPUT);
+  pinMode(PIN_ENABLE,OUTPUT);
+  pinMode(PIN_DIR,OUTPUT);
+  
+  pinMode(PIN_MOSFET0, OUTPUT);
+  pinMode(PIN_MOSFET1, OUTPUT);
+  
+  stepper_en=1;
+  digitalWrite(PIN_ENABLE,1-stepper_en);
+  
+  setupLCD();
+}
+
 void updateLCD()
 {
 //  lcd.clear();  
   lcd.setCursor(0,0);
   lcd.print(F("Speed: "));
-  lcd.print( (int)(motorSpeed) );
-  lcd.print(F(" rpm   "));
+  lcd.print( rotaryEncoderA.getValue() );
+  lcd.print(F(" %     "));
 
 /*  lcd.setCursor(0,1);
   lcd.print(F("Volume: "));//clear row
@@ -167,123 +181,101 @@ void updateLCD()
   const float pi =3.141593f;
   char strbuf[10];
   dtostrf(0.0f, 4, 2, strbuf);
-
   lcd.print(strbuf);
   lcd.print( " cm3/s");*/
-  
-  lcd.setCursor(0,1);
-  lcd.print("rot: ");
-  lcd.print(encoderValue);
-  lcd.print("   ");
 }
 
-
-void setMotorSpeed(float rpm)
-{
-  motorSpeed=rpm;
-  byte motorDir = rpm > 0 ? 1 : 0;
-  digitalWrite(MOTOR_DIR_PIN, motorDir);
-
-  updateLCD();
-
-  // Our motor has 1.8deg per step, so 360/1.8 is 200 steps per revolution
-  float stepsPerSecond = 200*rpm/60.0f*MICROSTEPS;
-
-  // set period in microseconds
-  if (stepsPerSecond != 0) {
-    Timer1.setPeriod(1e6/fabsf(stepsPerSecond));
-  }
-  
-  bool sleepMode = motorSpeed == 0.0f;
-  digitalWrite(MOTOR_SLEEP_PIN, sleepMode ? 0 : 1); // sleep pin is inverted
-
-  Serial.print(F("Steps per sec"));
-  Serial.println((int)stepsPerSecond);
-}
-
-void motorMove()
-{
-  digitalWrite(MOTOR_STEP_PIN,1);
-  digitalWrite(MOTOR_STEP_PIN,0);
-}
-
-
-/* *******************************************************
-/  Setup, this code is only executed once
-*/
-void setup() {
-  // Update clock
-  lastTick = millis();
-
-  // Open serial connection and print a message
-  Serial.begin(9600);
-  Serial.println(F("BioHack Academy Peristaltic Pump"));
-
-  // initialize the LED pin as an output:
-  pinMode(LED_PIN, OUTPUT);
-  
-  setupLCD();
-  
-  pinMode(MOTOR_DIR_PIN,OUTPUT);
-  pinMode(MOTOR_STEP_PIN,OUTPUT);
-  pinMode(MOTOR_SLEEP_PIN,OUTPUT);
-  digitalWrite(MOTOR_SLEEP_PIN,0); // sleep by default
-  
-  setupRotaryEncoder();
-  
-  delay(600);
-  Timer1.initialize(15000);
-  Timer1.attachInterrupt(motorMove); // blinkLED to run every 0.15 seconds
-  
-  updateLCD();
-}
-
-
-
-/* *******************************************************
-/  Loop, this code is constantly repeated
-*/
 void loop() {
-  // Update clock
-  uint32_t time = millis(); // current time since start of sketch
-  uint16_t dt = time-lastTick; // difference between current and previous time
-  lastTick = time;
-  
-  // Blink LED
+  // put your main code here
+  long time = millis();
+  long dt = time - last_time;
+  last_time = time;
+
   ledstate = 1-ledstate;
   digitalWrite(LED_PIN, ledstate); // alternate between 0 and 1   
 
-  // Update motor
-  if (time > lastUpdate + 100) {
-    if (encoderValue != 0) {
-      setMotorSpeed(motorSpeed + encoderValue); 
-      encoderValue=0;  
-    }
-    lastUpdate=time;
+  if(stepper_en) {  
+    stepper_timer+=dt;
+    update_timer+=dt;
+  }
+    
+  if (stepper_timer > step_time && stepper_en) {
+    stepper_timer-=step_time;
+    digitalWrite(PIN_STEP, 1);
+    delay(1);
+    digitalWrite(PIN_STEP, 0);
+    
+    steps++;
+  }
+  
+  mosfet_switch_timer+=dt;
+  if (mosfet_switch_timer > 1000) {
+    mosfet_switch_timer-=1000;
+    
+    mosfet_index=1-mosfet_index;
+    digitalWrite(PIN_MOSFET0, mosfet_index==0);
+    digitalWrite(PIN_MOSFET1, mosfet_index==1);
     updateLCD();
   }
   
-  // Respond to commands send over Serial
+  if (update_timer>300 && stepper_en) {
+    update_timer-=300;
+    Serial.print("steps: ");
+    Serial.print(steps);
+   
+    Serial.print("   A0: ");
+    Serial.print(analogRead(A0));
+
+    Serial.print("    A1: ");
+    Serial.print(analogRead(A1));
+    Serial.println();
+  }
+
+  if (rotaryEncoderA.poll()) {
+    Serial.print("Encoder A: ");
+    Serial.print(rotaryEncoderA.getValue());
+    Serial.println();
+    if(rotaryEncoderA.getValue() == 0) {
+      step_time = 10000000;
+    }
+    else {
+        step_time = 100/(rotaryEncoderA.getValue());
+    }
+  }
+  
+  if (rotaryEncoderB.poll()) {
+    Serial.print("Encoder B: ");
+    Serial.print(rotaryEncoderB.getValue());
+    Serial.println();
+  }
+      
   while (Serial.available()>0) {
     char c = (char)Serial.read();
     if (c == '\n') {
       if(buffer.startsWith("id")) {
         Serial.println("id:peristaltic-pump"); // so the bioreactor can figure out what is connected 
-      } else if (buffer.startsWith("move")) {
-        float sp = buffer.substring(4).toInt()/60.0f;
-        Serial.print(F("Setting motor speed to "));
-        Serial.print(sp, 2);
-        Serial.println(F(" rpm"));
-        setMotorSpeed(sp);
       }
-      else {
+      else if(buffer.startsWith("i2c")) {
+        scanI2C();
+      }    
+      else if(buffer.startsWith("sp")) {
+        int sp = buffer.substring(3).toInt();
+        
+        Serial.print("setting step time to : ");
+        Serial.println(sp);
+        
+        step_time = sp;
+      } else if (buffer.startsWith("en")) {
+        stepper_en = 1-stepper_en;
+        digitalWrite(PIN_ENABLE,1-stepper_en);
+        Serial.println((int)stepper_en);
+      } else {
         Serial.println("Unknown cmd.");
       }
       buffer="";
     } else buffer+=c;
-  }  
+  }
 }
-
 
 
 
